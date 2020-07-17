dff6ada5bae527d0b96f8143b60a2ed859f38984
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-17 21:10:43 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -23,14 +23,14 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         {
-          :name => "",
-          :age => "",
-          :attitude => ""
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
         },
         {
-          :name => "",
-          :age => "",
-          :attitude => ""
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
         }
       ]
    }
